eece2c803a59770f8af0f99132ce05ea440e42b1
Shisham Chudal <shishamchudal61@gmail.com>
2025-03-10 14:43:17 -0700

    Insert a document into 'customers' collection after establishing MongoDB connection

--- app.js
+++ app.js
@@ -12,10 +12,19 @@ client
     // client.close();
 
     var dbo = client.db("nodemongo");
-    dbo.createCollection("customers").then(function () {
-      console.log("Collection created");
-      //Close the database connection
-      client.close();
-    });
+    // dbo.createCollection("customers").then(function () {
+    //   console.log("Collection created");
+    //   //Close the database connection
+    //   client.close();
+    // });
+
+    var custData = { name: "Westcliff", address: "Irvine, CA" };
+    dbo
+      .collection("customers")
+      .insertOne(custData)
+      .then(function () {
+        console.log("1 document inserted");
+        client.close();
+      });
   })
   .catch((error) => console.log("Failed to connect", error));
