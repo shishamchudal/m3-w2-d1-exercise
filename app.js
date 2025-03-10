2c25c37b447314a7ad534408be81e084988277e9
Shisham Chudal <shishamchudal61@gmail.com>
2025-03-10 14:43:49 -0700

    Replace single document insertion with multiple document insertion for customers collection

--- app.js
+++ app.js
@@ -18,12 +18,37 @@ client
     //   client.close();
     // });
 
-    var custData = { name: "Westcliff", address: "Irvine, CA" };
+    // var custData = { name: "Westcliff", address: "Irvine, CA" };
+    // dbo
+    //   .collection("customers")
+    //   .insertOne(custData)
+    //   .then(function () {
+    //     console.log("1 document inserted");
+    //     client.close();
+    //   });
+
+    var custData = [
+      { name: "John", address: "Highway 71" },
+      { name: "Peter", address: "Lowstreet 4" },
+      { name: "Amy", address: "Apple st 652" },
+      { name: "Hannah", address: "Mountain 21" },
+      { name: "Michael", address: "Valley 345" },
+      { name: "Sandy", address: "Ocean blvd 2" },
+      { name: "Betty", address: "Green Grass 1" },
+      { name: "Richard", address: "Sky st 331" },
+      { name: "Susan", address: "One way 98" },
+      { name: "Vicky", address: "Yellow Garden 2" },
+      { name: "Ben", address: "Park Lane 38" },
+      { name: "William", address: "Central st 954" },
+      { name: "Chuck", address: "Main Road 989" },
+      { name: "Viola", address: "Sideway 1633" },
+    ];
+
     dbo
       .collection("customers")
-      .insertOne(custData)
-      .then(function () {
-        console.log("1 document inserted");
+      .insertMany(custData)
+      .then(function (res) {
+        console.log("Number of documents inserted:" + res.insertedCount);
         client.close();
       });
   })
